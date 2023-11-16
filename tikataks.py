3f498f1bf1ae0c041564175c1fd3fe83281967b9
FreeNFF <nauisfricsons30@gmail.com>
2023-11-16 14:52:10 +0200

    2. stunda
    2. stunda

--- tikataks.py
+++ tikataks.py
@@ -6,6 +6,11 @@ from tkinter import messagebox #paziņojumi, ieteikumi
 mansLogs=Tk()# loga objekts
 mansLogs.title("Desas")
 
+##########################################################################################################
+
+count=0
+speletajsX=True
+winner=False
 ######################################################################################################
 
 def btnClick(button):
@@ -14,19 +19,42 @@ def btnClick(button):
         button["text"]="X"#maina uz X
         speletajsX=False
         count+=1#palielina rūtiņu skaitu
+        checkWinner()
+    elif button["text"]=='' and speletajsX==False:
+        button["text"]='O'
+        speletajsX=True
+        count+=1
+        checkWinner()
+    else:
+        messagebox.showerror('Desas', 'Šeit kāds ir ieklikšķinājis.')
+    return
 
 #############################################################################################################
 
+def checkWinner():#funkcija, kas nosaka uzvarētāju
+    global winner
+    winner=False
+    if (btn1["text"]=="X"and btn2["text"]=="X" and btn3["text"]=="X" or btn4["text"]=="X"and btn5["text"]=="X" and btn6["text"]=="X" or btn7["text"]=="X"and btn8["text"]=="X" and btn9["text"]=="X" or btn1["text"]=="X"and btn4["text"]=="X" and btn7["text"]=="X" or btn2["text"]=="X"and btn5["text"]=="X" and btn8["text"]=="X" or btn3["text"]=="X"and btn6["text"]=="X" and btn9["text"]=="X" or btn1["text"]=="X"and btn5["text"]=="X" and btn9["text"]=="X" or btn3["text"]=="X"and btn5["text"]=="X" and btn7["text"]=="X"):
+        winner=True
+        messagebox.showinfo("Desas", "1. spēlētajs ir uzvarētājs.")
+    elif(btn1["text"]=="O"and btn2["text"]=="O" and btn3["text"]=="O" or btn4["text"]=="O"and btn5["text"]=="O" and btn6["text"]=="O" or btn7["text"]=="O"and btn8["text"]=="O" and btn9["text"]=="O" or btn1["text"]=="O"and btn4["text"]=="O" and btn7["text"]=="O" or btn2["text"]=="O"and btn5["text"]=="O" and btn8["text"]=="O" or btn3["text"]=="O"and btn6["text"]=="O" and btn9["text"]=="O" or btn1["text"]=="O"and btn5["text"]=="O" and btn9["text"]=="O" or btn3["text"]=="O"and btn5["text"]=="O" and btn7["text"]=="O"):
+        winner=True
+        messagebox.showinfo("Desas", "2. spēlētajs ir uzvarētājs.")
+    elif count==9 and winner==False:
+        messagebox.showinfo("Desas", "Neizšķirts.")
+    return
 
-btn1= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="light yellow")
-btn2= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="yellow")
-btn3= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="light yellow")
-btn4= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="yellow")
-btn5= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="light yellow")
-btn6= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="yellow")
-btn7= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="light yellow")
-btn8= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24), bg="yellow")
-btn9= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="light yellow")
+################################################################################################################
+
+btn1= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="light yellow", command=lambda:btnClick(btn1))
+btn2= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="yellow", command=lambda:btnClick(btn2))
+btn3= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="light yellow", command=lambda:btnClick(btn3))
+btn4= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="yellow", command=lambda:btnClick(btn4))
+btn5= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="light yellow", command=lambda:btnClick(btn5))
+btn6= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="yellow", command=lambda:btnClick(btn6))
+btn7= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="light yellow", command=lambda:btnClick(btn7))
+btn8= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24), bg="yellow", command=lambda:btnClick(btn8))
+btn9= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="light yellow", command=lambda:btnClick(btn9))
 
 
 
