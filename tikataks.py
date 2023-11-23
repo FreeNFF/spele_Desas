90a93a20f451ca06c65aeb1417d7e94c41381232
FreeNFF <nauisfricsons30@gmail.com>
2023-11-23 14:37:10 +0200

    3. stunda
    3. stunda

--- tikataks.py
+++ tikataks.py
@@ -6,16 +6,59 @@ from tkinter import messagebox #paziņojumi, ieteikumi
 mansLogs=Tk()# loga objekts
 mansLogs.title("Desas")
 
-##########################################################################################################
 
+#Disabled - izslēgta poga, Active – uz pogas ir noklikšķināts, Normal – parasts pogas stāvoklis, ir pieejama
+
+
+##########################################################################################################
+global winner, speletajsX, count
 count=0
 speletajsX=True
 winner=False
 ######################################################################################################
 
+def disableButtons(): #spēle beidzas, pogas izslēgtas
+ btn1.config(state=DISABLED) #pogas stāvoklis izslēgts
+ btn2.config(state=DISABLED)
+ btn3.config(state=DISABLED)
+ btn4.config(state=DISABLED)
+ btn5.config(state=DISABLED)
+ btn6.config(state=DISABLED)
+ btn7.config(state=DISABLED)
+ btn8.config(state=DISABLED)
+ btn9.config(state=DISABLED)
+ return
+
+########################################################################################################
+
+def reset():
+    count=0
+    btn1.config(state=NORMAL)
+    btn2.config(state=NORMAL)
+    btn3.config(state=NORMAL)
+    btn4.config(state=NORMAL)
+    btn5.config(state=NORMAL)
+    btn6.config(state=NORMAL)
+    btn7.config(state=NORMAL)
+    btn8.config(state=NORMAL)
+    btn9.config(state=NORMAL)
+    btn1["text"]=""
+    btn2["text"]=""
+    btn3["text"]=""
+    btn4["text"]=""
+    btn5["text"]=""
+    btn6["text"]=""
+    btn7["text"]=""
+    btn8["text"]=""
+    btn9["text"]=""
+    return
+
+
+#######################################################################################################3
+
 def btnClick(button):
     global speletajsX,count#kādi mainīgie tiks izmantoti visā programmā(global mainīgie-izmantoti viša programmā)
-    if button["text"]==""and speletajsX==True:#spļē X spēlētājs
+    if button["text"]==""and speletajsX==True:#spēlē X spēlētājs
         button["text"]="X"#maina uz X
         speletajsX=False
         count+=1#palielina rūtiņu skaitu
@@ -36,11 +79,14 @@ def checkWinner():#funkcija, kas nosaka uzvarētāju
     winner=False
     if (btn1["text"]=="X"and btn2["text"]=="X" and btn3["text"]=="X" or btn4["text"]=="X"and btn5["text"]=="X" and btn6["text"]=="X" or btn7["text"]=="X"and btn8["text"]=="X" and btn9["text"]=="X" or btn1["text"]=="X"and btn4["text"]=="X" and btn7["text"]=="X" or btn2["text"]=="X"and btn5["text"]=="X" and btn8["text"]=="X" or btn3["text"]=="X"and btn6["text"]=="X" and btn9["text"]=="X" or btn1["text"]=="X"and btn5["text"]=="X" and btn9["text"]=="X" or btn3["text"]=="X"and btn5["text"]=="X" and btn7["text"]=="X"):
         winner=True
+        disableButtons()
         messagebox.showinfo("Desas", "1. spēlētajs ir uzvarētājs.")
     elif(btn1["text"]=="O"and btn2["text"]=="O" and btn3["text"]=="O" or btn4["text"]=="O"and btn5["text"]=="O" and btn6["text"]=="O" or btn7["text"]=="O"and btn8["text"]=="O" and btn9["text"]=="O" or btn1["text"]=="O"and btn4["text"]=="O" and btn7["text"]=="O" or btn2["text"]=="O"and btn5["text"]=="O" and btn8["text"]=="O" or btn3["text"]=="O"and btn6["text"]=="O" and btn9["text"]=="O" or btn1["text"]=="O"and btn5["text"]=="O" and btn9["text"]=="O" or btn3["text"]=="O"and btn5["text"]=="O" and btn7["text"]=="O"):
         winner=True
+        disableButtons()
         messagebox.showinfo("Desas", "2. spēlētajs ir uzvarētājs.")
     elif count==9 and winner==False:
+        disableButtons()
         messagebox.showinfo("Desas", "Neizšķirts.")
     return
 
@@ -59,6 +105,20 @@ btn9= Button(mansLogs, text="", width=6, height=3,font=('Helvica',24),bg="light 
 
 
 ###########################################################################################################################
+
+#Lielā izvēle
+galvenaIzvele=Menu(mansLogs)#izveido galveno izvēli
+mansLogs.config(menu=galvenaIzvele)#pievieno galvenajam logam
+
+#Mazā izvēle
+opcijas=Menu(galvenaIzvele,tearoff=False)#mazā izvēle
+galvenaIzvele.add_cascade(label="Opcijas", menu=opcijas)#lejupkrītošais saraksts
+
+#Komandas
+opcijas.add_command(label="Jauna spēle", command=reset)
+opcijas.add_command(label="Iziet", command=mansLogs.quit)
+
+############################################################################################################################
 
 btn1.grid(row=0,column=0)
 btn2.grid(row=1,column=0)
